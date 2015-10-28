b69d3d02b323823c71ef3fbb5177c99d8740945e
Toni Anne Romero <tonianne.romero@gmail.com>
2015-10-28 14:18:47 +0800

    in class coding

---
+++ first_app/app/controllers/tweets_controller.rb
@@ -0,0 +1,42 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def create
+    @tweet = Tweet.find(params[:id])
+    @tweet.create(tweet_params)
+    redirect_to @tweet
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(tweet_params)
+    redirect_to @tweet
+  end
+
+  def destroy
+    @tweet = Tweet.find(paramd[:id])
+    @tweet.destroy
+    redirect_to tweets_url
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+
+end
--- first_app/config/routes.rb
+++ first_app/config/routes.rb
@@ -1,56 +1,26 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # resources :post, only: [:index, :show]
+  # get 'posts/somthing_special', to: "post#something_special"
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #index
+  get '/tweets', to: "tweets#index"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #show
+  get '/tweets/:id', to: "tweets#show", as: "tweet"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #new
+  get '/tweets/new', to: "tweets#new"
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  #edit
+  get '/tweets/:id/edit', to: "tweets#edit", as: "edit_tweet"
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  #create
+  post '/tweets', to: "tweets#create"
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  #update
+  put '/tweets/:id', to: "tweets#update"
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  #destroy
+  delete '/tweets/:id', to: "tweets#destroy"
+resources :posts
 end
